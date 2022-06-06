f5d11640ebc5c3ee80d71425853c532d368da5b5
Szekely Bendeguz <bendeguz.szekely@egroup.hu>
2022-06-06 10:32:54 +0200

    Initial Commit

---
+++ Shared/AppetizersFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersFinalApp.swift
+//  Shared
+//
+//  Created by Deim Peter Pal on 2022. 06. 06..
+//
+
+import SwiftUI
+
+@main
+struct AppetizersFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
